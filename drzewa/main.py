c3b8a0fad2b97ac57c9543e0700626d2099b91cd
Marcin <marcinkrueger@onet.eu>
2023-03-10 00:04:49 +0100

    building tree

--- drzewa/main.py
+++ drzewa/main.py
@@ -7,6 +7,7 @@ def read_data(path: str):
 
 def crossvalidation(data, k: int = 10):
     print(np.shape(data)[0])
+
     subset_size = np.shape(data)[0] // k
 
     for i in range(k):
@@ -17,43 +18,77 @@ def predict(no: int):
     y = read_data(f"./data/{no}-Y.csv")
     data = pd.concat([X, y], axis=1).values
     # crossvalidation(data, 10)
-    find_division(data, 1)
+    split_mse, split_val, split_col = find_split(data)
+    return split_val, split_col
 
 def mse(y_true, y_pred):
     return np.mean((y_pred - y_true)**2)
 
-def find_division(data: np.ndarray, col: int):
-    sorted_indexes = np.argsort(data[:, col])
-    sorted_data = data[sorted_indexes]
-    _, unique_indices = np.unique(sorted_data[:, col], return_index=True)
+def find_split(data: np.ndarray):
     min_loss = None
     div_value = None
+    min_col = None
+    population = np.shape(data)[0]
+    for col in range(np.shape(data)[1] - 1):
+        sorted_indexes = np.argsort(data[:, col])
+        sorted_data = data[sorted_indexes]
+        _, unique_indices = np.unique(sorted_data[:, col], return_index=True)
 
-    for index in unique_indices[1:]:
-        left = sorted_data[:index]
-        right = sorted_data[index:]
+        for index in unique_indices[1:]:
+            left = sorted_data[:index]
+            right = sorted_data[index:]
 
-        loss = min(mse(np.mean(left[:, -1]), sorted_data[:, -1]), \
-            mse(np.mean(right[:, -1]), sorted_data[:, -1]))
+            loss = min(mse(np.mean(left[:, -1]), sorted_data[:, -1]), \
+                mse(np.mean(right[:, -1]), sorted_data[:, -1]))
 
-        if min_loss == None or loss < min_loss:
-            min_loss = loss
-            div_value = sorted_data[index, col]
-            print()
+            if (min_loss == None or loss < min_loss) and np.shape(left)[0] / population > 0.1 and np.shape(right)[0] / population > 0.1:
+                min_loss = loss
+                div_value = sorted_data[index, col]
+                min_col = col
     
-    return min_loss, div_value
+    return min_loss, div_value, min_col
 
 predict(1)
 
 
 
-class Node(object):
-    def __init__(self):
+class Node():
+    def __init__(self, data, level=0, max_level=5, is_leaf=False):
         self.left = None  # Typ: Node, wierzchoĹek znajdujÄcy siÄ po lewej stornie
         self.right = None  # Typ: Node, wierzchoĹek znajdujÄcy siÄ po prawej stornie
+        self.is_leaf = is_leaf
+        self.level = level
+        self.max_level = max_level
+        self.col = None
+        self.value = None
+        self.predict_value = 0
+        self.data = data
 
-    def perform_split(self, data):
-        pass
+
+    def perform_split(self):
+        if self.level >= self.max_level:
+            self.is_leaf = True
+            self.predict_value = np.mean(self.data[:, -1])
+            print(self.predict_value)
+        else:
+            _, self.value, self.col = find_split(self.data)
+            print(self.value)
+            print(self.col)
+            # print(np.shape(self.data))
+            condition = self.data[:, self.col] < self.value
+            print(condition)
+            left_data = self.data[condition]
+            right_data = self.data[~condition]
+
+            print(np.shape(left_data))
+            print(np.shape(right_data))
+
+            self.left = Node(left_data, self.level + 1, self.max_level)
+            self.right = Node(right_data, self.level + 1, self.max_level)
+
+            self.left.perform_split()
+            self.right.perform_split()
+
     # ZnajdĹş najlepszy podziaĹ data
     # if uzyskano poprawÄ funkcji celu (bÄdĹş inny, zaproponowany przez Ciebie warunek):
     # podziel dane na dwie czÄĹci d1 i d2, zgodnie z warunkiem
@@ -65,50 +100,18 @@ class Node(object):
     # obecny Node jest liĹciem, zapisz jego odpowiedĹş
 
     def predict(self, example):
-        pass
-        """
-        if not Node jest liĹciem:
-            if warunek podziaĹu jest speĹniony:
-                return self.right.predict(example)
+        if self.is_leaf == True:
+            print("XDDDD")
+            return self.predict_value
+        else:
+            if example[self.col] <= self.value:
+                self.left.predict(example)
             else:
-                return self.left.predict(example)
-        return zwrĂłÄ wartoĹÄ (Node jest liĹciem)
-        """
+                self.right.predict(example)
 
-def Tree():
-    pass
+# X = read_data(f"./data/1-X.csv")
+# y = read_data(f"./data/1-Y.csv")
+# data = pd.concat([X, y], axis=1).values
 
-# Najprostsze wczytywanie i zapisywanie danych, a takĹźe wywoĹanie obiektu Node
-# SzczegĂłlnie przydatne dla osĂłb nie znajÄcych numpy
-# ZbiĂłr danych jest reprezentowany jako "lista list".
-# tj. data[0] zwrĂłci listÄ z wartoĹciami cech pierwszego przykĹadu
-# JeĹli znasz numpy i bolÄ CiÄ od poniĹźszego kodu oczy - moĹźesz go zmieniÄ
-# JeĹli nie znasz numpy - skorzystaj z kodu, dokoĹcz zadanie... i naucz sie numpy. W kolejnych tygodniach bÄdziemy z niego korzystaÄ.
-
-# id = "1"  # podaj id zbioru danych ktĂłry chcesz przetworzyÄ np. 1
-# data = []
-# path = "./data/"
-# y = [line.strip() for line in open(path + id + '-Y.csv')]
-# for i, line in enumerate(open(path + id + '-X.csv')):
-#     if i == 0:
-#         continue
-#     x = [float(j) for j in line.strip().split(',')]
-#     nAttr = len(x)
-#     x.append(float(y[i]))
-#     data.append(x)
-#
-# print(data)
-
-print('Data load complete!')
-# tree_root = Node()
-# tree_root.perform_split(data)
-# print('Training complete!')
-#
-# with open(id + '.csv', 'w') as f:
-#     for i, line in enumerate(open(id + '-test.csv')):
-#         if i == 0:
-#             continue
-#         x = [float(j) for j in line.strip().split(',')]
-#         y = tree.predict(x)
-#         f.write(str(y))
-#         f.write('\n')
+# tree = Node(data)
+# tree.perform_split()

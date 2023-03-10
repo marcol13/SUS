bae3de02e1cd448388a7af510d37707b74f6cbb8
Marcin <marcinkrueger@onet.eu>
2023-03-10 02:44:54 +0100

    perform tests to find optimal value of max trees level

--- drzewa/main.py
+++ drzewa/main.py
@@ -10,7 +10,7 @@ def predict(no: int):
     y = read_data(f"./data/{no}-Y.csv")
     data = pd.concat([X, y], axis=1).values
     # crossvalidation(data, 10)
-    split_mse, split_val, split_col = find_split(data)
+    _, split_val, split_col = find_split(data)
     return split_val, split_col
 
 def mse(y_true, y_pred):
@@ -26,6 +26,9 @@ def find_split(data: np.ndarray):
         sorted_data = data[sorted_indexes]
         _, unique_indices = np.unique(sorted_data[:, col], return_index=True)
 
+        if len(unique_indices) < 2:
+            continue
+
         for index in unique_indices[1:]:
             left = sorted_data[:index]
             right = sorted_data[index:]
@@ -33,7 +36,7 @@ def find_split(data: np.ndarray):
             loss = min(mse(np.mean(left[:, -1]), sorted_data[:, -1]), \
                 mse(np.mean(right[:, -1]), sorted_data[:, -1]))
 
-            if (min_loss == None or loss < min_loss) and np.shape(left)[0] / population > 0.1 and np.shape(right)[0] / population > 0.1:
+            if (min_loss == None or loss < min_loss) and np.shape(left)[0] / population > 0.35 and np.shape(right)[0] / population > 0.35:
                 min_loss = loss
                 div_value = sorted_data[index, col]
                 min_col = col
@@ -63,26 +66,20 @@ class Node():
             self.predict_value = np.mean(self.data[:, -1])
         else:
             _, self.value, self.col = find_split(self.data)
-            condition = self.data[:, self.col] < self.value
-            
-            left_data = self.data[condition]
-            right_data = self.data[~condition]
+            if self.col == None:
+                self.is_leaf = True
+                self.predict_value = np.mean(self.data[:, -1])
+            else:
+                condition = self.data[:, self.col] < self.value
+                
+                left_data = self.data[condition]
+                right_data = self.data[~condition]
 
-            self.left = Node(left_data, self.level + 1, self.max_level)
-            self.right = Node(right_data, self.level + 1, self.max_level)
+                self.left = Node(left_data, self.level + 1, self.max_level)
+                self.right = Node(right_data, self.level + 1, self.max_level)
 
-            self.left.perform_split()
-            self.right.perform_split()
-
-    # ZnajdĹş najlepszy podziaĹ data
-    # if uzyskano poprawÄ funkcji celu (bÄdĹş inny, zaproponowany przez Ciebie warunek):
-    # podziel dane na dwie czÄĹci d1 i d2, zgodnie z warunkiem
-    # self.left = Node()
-    # self.right = Node()
-    # self.left.perform_split(d1)
-    # self.right.perform_split(d2)
-    # else:
-    # obecny Node jest liĹciem, zapisz jego odpowiedĹş
+                self.left.perform_split()
+                self.right.perform_split()
 
     def predict(self, example):
         if self.is_leaf == True:
@@ -93,16 +90,7 @@ class Node():
             else:
                 return self.right.predict(example)
 
-# X = read_data(f"./data/1-X.csv")
-# y = read_data(f"./data/1-Y.csv")
-# data = pd.concat([X, y], axis=1).values
-
-# tree = Node(data)
-# tree.perform_split()
-
-def crossvalidation(data, k: int = 10):
-    print(np.shape(data)[0])
-
+def crossvalidation(data, max_level: int, k: int = 10):
     subset_size = np.shape(data)[0] // k
     rmse_total = []
 
@@ -114,7 +102,7 @@ def crossvalidation(data, k: int = 10):
         validation = data[val_indices]
         train = data[train_indices]
 
-        tree = Node(train)
+        tree = Node(train, max_level=max_level)
         tree.perform_split()
 
         rmse_avg = None
@@ -132,8 +120,41 @@ def crossvalidation(data, k: int = 10):
             
     print(np.mean(rmse_total))
 
+def test(no, max_level):
+    X = read_data(f"./data/{no}-X.csv")
+    y = read_data(f"./data/{no}-Y.csv")
+    data = pd.concat([X, y], axis=1).values
+    crossvalidation(data, max_level)
+    print(no)
+    print(max_level)
 
-X = read_data(f"./data/1-X.csv")
-y = read_data(f"./data/1-Y.csv")
-data = pd.concat([X, y], axis=1).values
-crossvalidation(data)
+# test(13, 9)
+
+#empirycznie wyznaczone maksymalne wysokości drzew (za pomocą "ręcznego" grid searchu z wykorzystaniem cross-walidacji)
+levels = [5, 8, 6, 7, 7, 8, 6, 6, 7, 6, 5, 4, 9]
+
+rmse = [0.667, 
+        0.374,
+        0.743,
+        0.794,
+        0.349, 
+        0.567,
+        0.265,
+        0.446,
+        0.789,
+        0.791,
+        0.843,
+        0.686,
+        0.570]
+
+def evaluate(levels):
+    for no in range(1,14):
+        X = read_data(f"./data/{no}-X.csv")
+        y = read_data(f"./data/{no}-Y.csv")
+        test = read_data(f"./data/{no}-test.csv")
+        data = pd.concat([X, y], axis=1).values
+
+        tree = Node(data)
+        tree.perform_split()
+
+evaluate(levels)
